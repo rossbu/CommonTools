2e52e43f3dac3d45fdba2dbb78b40f404df326e3
rossbu <smartequipct@gmail.com>
2019-12-02 16:16:27 -0500

    Tutorial-2: Optional Demo

--- src/main/java/com/jdk/jdk8/OptionalDemo.java
+++ src/main/java/com/jdk/jdk8/OptionalDemo.java
@@ -1,6 +1,9 @@
 package com.jdk.jdk8;
 
 import com.pojo.Person;
+import com.pojo.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
@@ -17,24 +20,76 @@ import java.util.Optional;
  */
 public class OptionalDemo {
     public static void main(String[] args) {
-        Optional<String> optional =  Optional.empty();
-        System.out.println(optional);
+        emptyOptional();
+        ifPresent();
+        orElse();
+        ifPresentOrElse();  // jdk9
+        ofNullable();  // turn null -> empty optional object
+    }
 
-        // If you still use the get() functionality on optionals, you're effectively opening youselvies to the same problems as null checks.
-        // Don't use Optional get without isPresent
-        System.out.println(optional.get());
+    private static void ofNullable() {
+//        If the specified value is null, then this method returns an empty instance of the Optional class.
+        Object o = null;
+        Optional op1  = Optional.ofNullable(o);
+        System.out.println("Optional 1: " + op1);
+    }
 
+    private static void ifPresentOrElse() {
+        Optional<Integer> op = Optional.of(131);
+        op.ifPresentOrElse(( var e ) -> {
+            System.out.println("yes, i have value: " + e);
+        }, () -> {
+            System.out.println("no, I have nothing here");
+        });
+
+
+        op = Optional.empty();
+        op.ifPresentOrElse(( var e ) -> {
+            System.out.println("yes, i have value: " + e);
+        }, () -> {
+            System.out.println("no, I have nothing here");
+        });
+    }
+
+    private static void orElse() {
+        // use ifpresent otherwise like Optional.ofNullable<List>.map(Collection::stream).findfirst().ifpresent ( lllambda)
+        Optional<Person> opt = Optional.empty();
+        Person x = opt.orElse( new Person() );
+        Person y = opt.orElseGet( Person::new ); // lazy loading for heavy resource
+    }
+
+    private static void ifPresent() {
         Optional<String> petNameOptional = Optional.of("Bobby");
         String petName = "";
         if(petNameOptional.isPresent()){
             petName = petNameOptional.get();
         }
+    }
 
-        Optional<Person> opt = Optional.empty();
-        Person x = opt.orElse( new Person() );
-        Person y = opt.orElseGet( Person::new ); // lazy loading for heavy resource
+    private static void emptyOptional() {
+        Optional<String> optional =  Optional.empty();
+        System.out.println(optional);
+        // If you still use the get() functionality on optionals, you're effectively opening youselvies to the same problems as null checks. Don't use Optional get without isPresent
+//        System.out.println(optional.get());
+        // Do NOT do this , same as null pointer exception ,use ifPresent first, then get if true.
+    }
 
-        // use ifpresent otherwise like Optional.ofNullable<List>.map(Collection::stream).findfirst().ifpresent ( lllambda)
-
+    @RequestMapping(value = {"/article", "/article/{id}}" })
+    public User springOptionalTest(@PathVariable Optional<Integer> optionalArticleId) {
+        if (optionalArticleId.isPresent()) {
+            Integer articleId = optionalArticleId.get();
+        } else {
+            ;
+        }
+        return null;
+    }
+    @RequestMapping(value = {"/user", "/user/{id}"})
+    public User getUser(@PathVariable(required = false) Integer userId) {
+        if (userId != null) {
+            //...
+        } else {
+            //...
+        }
+        return new User();
     }
 }
