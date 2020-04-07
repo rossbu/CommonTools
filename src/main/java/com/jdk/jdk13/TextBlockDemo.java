082653d9065e8ee84f3f8fbd7e5c6d91153ddd15
rossbu <ross.bu@gmail.com>
2020-04-06 20:14:55 -0400

    Modify test case with JDK14 feature

--- src/main/java/com/jdk/jdk13/TextBlockDemo.java
+++ src/main/java/com/jdk/jdk13/TextBlockDemo.java
@@ -3,8 +3,11 @@ package com.jdk.jdk13;
 
 public class TextBlockDemo {
     public static void main(String[] args) {
-//        String json = """
-//        test me
-//        """;
+        String json = """
+        test me \s
+        test me again\s
+        haha
+        """;
+        System.out.println(json);
     }
 }
